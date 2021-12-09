a3d29eb968f0129039e8584ab2f69fdb2cbc13b8
hannyiszoltan <hannyiszoltan@gmail.com>
2021-12-09 12:47:38 +0100

    design_navbar_kartyaprobalas

--- javascript/film_adatok.js
+++ javascript/film_adatok.js
@@ -7,8 +7,26 @@ fetch('http://zarodolgozat.test/backend/json_lekerdezes.php')
 function megjelenit(adatok){
 console.log(adatok);
 var i=1;
+let card_content="";
 for (var elem of adatok){
+    card_content+='<div class="card" style="width: 18rem; margin-left: 30px">';
+    card_content+='<img src="" class="card-img-top" alt="film_kép">';
+    card_content+='<div class="card-body">';
+    card_content+='<h5 class="card-title">'+elem.film_cim+'</h5>';
+    card_content+='<p class="card-text">'+elem.film_leiras+'</p>';
+    card_content+='</div>';
+    card_content+='<ul class="list-group list-group-flush">';
+    card_content+='<li class="list-group-item">'+"An item"+'</li>';
+    card_content+='<li class="list-group-item">'+"A second item"+'</li>';
+    card_content+='<li class="list-group-item">'+"A third item"+'</li>';
+    card_content+='</ul>';
+    card_content+='<div class="card-body">';
+    card_content+='<a href="#" class="card-link btn btn-outline-success">'+"Bővebben"+'</a>';
+    card_content+='<a href="#" class="card-link btn btn-outline-success">'+"Vélemények"+'</a>';
+    card_content+='</div>';
+    card_content+='</div>';
 
+    document.getElementById("tablazat").innerHTML=sz;
 
 
 }
--- kezdolap.php
+++ kezdolap.php
@@ -12,6 +12,7 @@ include "./backend/bejelentkezes_ell.php";
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <link rel="stylesheet" href="./CSS/kezdolap.css">
     <!-- Latest compiled and minified CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
@@ -24,67 +25,69 @@ include "./backend/bejelentkezes_ell.php";
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#">Kezdőlap</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="#">Kedvencek</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="#">Forum</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
+                        Toplisták
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Kaland</a></li>
                         <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><a class="dropdown-item" href="#">Akció</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Fantasy</a></li>
                     </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
                 </li>
             </ul>
             <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Bejelentkezve: <?php echo $_SESSION["email"]; ?></a>
+
+                <p><a class="btn btn-outline-danger" href='./backend/kijelentkezes.php'>Kijelentkezés</a></p>
             </form>
         </div>
     </div>
 </nav>
 
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores illum ipsa neque nesciunt numquam optio reprehenderit ullam vitae. Aperiam doloribus ea ex facere ipsum odio odit praesentium similique unde veritatis.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque consectetur, corporis doloremque est eveniet fugit impedit magnam maxime minus molestias quasi, quibusdam quo reiciendis sint soluta velit veniam. Deserunt, quisquam.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid quasi recusandae rem voluptatibus! Ab aut, delectus esse exercitationem fuga illo maxime nesciunt nobis quasi, qui quia quo sint, unde voluptates?
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque esse officia possimus sit tenetur voluptas voluptates? Commodi consectetur, dignissimos doloremque ipsum maiores minima obcaecati officia, perspiciatis ratione repellat soluta velit?
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dolores eligendi error nesciunt odit pariatur quam quas suscipit tempora vitae. Aliquam culpa nisi perspiciatis placeat porro possimus quasi sint soluta.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores illum ipsa neque nesciunt numquam optio reprehenderit ullam vitae. Aperiam doloribus ea ex facere ipsum odio odit praesentium similique unde veritatis.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque consectetur, corporis doloremque est eveniet fugit impedit magnam maxime minus molestias quasi, quibusdam quo reiciendis sint soluta velit veniam. Deserunt, quisquam.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid quasi recusandae rem voluptatibus! Ab aut, delectus esse exercitationem fuga illo maxime nesciunt nobis quasi, qui quia quo sint, unde voluptates?
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque esse officia possimus sit tenetur voluptas voluptates? Commodi consectetur, dignissimos doloremque ipsum maiores minima obcaecati officia, perspiciatis ratione repellat soluta velit?
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dolores eligendi error nesciunt odit pariatur quam quas suscipit tempora vitae. Aliquam culpa nisi perspiciatis placeat porro possimus quasi sint soluta.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores illum ipsa neque nesciunt numquam optio reprehenderit ullam vitae. Aperiam doloribus ea ex facere ipsum odio odit praesentium similique unde veritatis.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque consectetur, corporis doloremque est eveniet fugit impedit magnam maxime minus molestias quasi, quibusdam quo reiciendis sint soluta velit veniam. Deserunt, quisquam.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid quasi recusandae rem voluptatibus! Ab aut, delectus esse exercitationem fuga illo maxime nesciunt nobis quasi, qui quia quo sint, unde voluptates?
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque esse officia possimus sit tenetur voluptas voluptates? Commodi consectetur, dignissimos doloremque ipsum maiores minima obcaecati officia, perspiciatis ratione repellat soluta velit?
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dolores eligendi error nesciunt odit pariatur quam quas suscipit tempora vitae. Aliquam culpa nisi perspiciatis placeat porro possimus quasi sint soluta.
 
 
-<div className="card" style="width: 18rem;">
-    <img src="..." className="card-img-top" alt="...">
-    <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
-            card's content.</p>
-    </div>
-    <ul className="list-group list-group-flush">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-    </ul>
-    <div className="card-body">
-        <a href="#" className="card-link">Card link</a>
-        <a href="#" className="card-link">Another link</a>
-    </div>
+<div id="tablazat">
+
 </div>
 
 
 
-<p><a href='./backend/kijelentkezes.php'>Kijelentkezés</a></p>
 
 
+
+<footer>Designed by: H.Z.</footer>
 </body>
 </html>
