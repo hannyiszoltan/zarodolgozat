90ec631fd816532e93247ffc08bb5c27de91a07b
hannyiszoltan <hannyiszoltan@gmail.com>
2022-01-31 10:04:46 +0100

    adatbazis_adatok_modositasa_onlinera

--- backend/db_connection.php
+++ backend/db_connection.php
@@ -1,8 +1,8 @@
 <?php
-$host="localhost:3306";
-$user="root";
-$pass="";
-$db="film";
+$host="sql213.epizy.com";
+$user="epiz_30949995";
+$pass="yTh2MGo34R";
+$db="epiz_30949995_film";
 
 
 $conn=mysqli_connect($host, $user, $pass, $db);
